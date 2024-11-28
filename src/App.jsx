0d57395d1c4a595b5164d2ea7bc1e93b7f1e3f94
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-11-28 15:50:26 +0100

    add component header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
+
+import Header from "./components/Header";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./css/App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
